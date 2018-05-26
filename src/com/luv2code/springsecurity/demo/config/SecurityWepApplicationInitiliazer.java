127786390c68a21ef10f8bfe5beea4a395ad20b5
KaanCaymaz <kaancaymaz82@gmail.com>
2018-05-26 12:18:13 +0300

    Initial commit

---
+++ src/com/luv2code/springsecurity/demo/config/SecurityWepApplicationInitiliazer.java
@@ -0,0 +1,10 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class SecurityWepApplicationInitiliazer extends AbstractSecurityWebApplicationInitializer{
+
+}
